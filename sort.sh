b14c3c0884ce2cc41c3db9cc95e259b3772ff9a0
purushothaman-source <purushoth99jun@gmail.com>
2020-11-16 18:44:53 +0530

    ADDED:stored in array

--- sort.sh
+++ sort.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/bin/bash 
+i=0
 read -p "enter a" a
 read -p "enter b" b
 read -p "enter c" c
@@ -7,5 +8,12 @@ number2=$((a*b+c))
 number3=$((c+a/b))
 number4=$((a%b+c))
 declare -A  values=( [0]=$number1 [1]=$number2 [2]=$number3 [3]=$number4 )
-echo "Stored numbers:${values[@]}"
+echo "Stored numbers in dictionary:${values[@]}"
+for key in "${!values[@]}"
+do
+    
+        arr[i]="${values[$key]}"
+       ((i++))
+done
+echo "stored in array :${arr[@]}"
 
