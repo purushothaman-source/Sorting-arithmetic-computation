abab71115085fcbe400a8094addcb26c064e2418
purushothaman-source <purushoth99jun@gmail.com>
2020-11-16 18:13:32 +0530

    stored in dictionary

--- sort.sh
+++ sort.sh
@@ -2,5 +2,10 @@
 read -p "enter a" a
 read -p "enter b" b
 read -p "enter c" c
-number=$((a%b+c))
-echo "Number:$number"
+number1=$((a+b*c))
+number2=$((a*b+c))
+number3=$((c+a/b))
+number4=$((a%b+c))
+declare -A  values=( [0]=$number1 [1]=$number2 [2]=$number3 [3]=$number4 )
+echo "Stored numbers:${values[@]}"
+
