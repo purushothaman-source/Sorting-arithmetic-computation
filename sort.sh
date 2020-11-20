ba6d2f130e065dfa359699d8701f845fa9754f48
purushothaman-source <purushoth99jun@gmail.com>
2020-11-20 15:39:09 +0530

    Added:sorted ascending order

--- sort.sh
+++ sort.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-i=0
 read -p "enter a" a
 read -p "enter b" b
 read -p "enter c" c
@@ -7,13 +6,23 @@ number1=$((a+b*c))
 number2=$((a*b+c))
 number3=$((c+a/b))
 number4=$((a%b+c))
-declare -A  values=( [0]=$number1 [1]=$number2 [2]=$number3 [3]=$number4 )
+values=( [0]=$number1 [1]=$number2 [2]=$number3 [3]=$number4 )
 echo "Stored numbers in dictionary:${values[@]}"
-for key in "${!values[@]}"
+for ((i = 0; i<${#values[@]}; i++))
 do
-    
-        arr[i]="${values[$key]}"
-       ((i++))
-done
-echo "stored in array :${arr[@]}"
 
+        for((j = 0; j<${#values[@]}-i-1; j++))
+        do
+
+                if [[ ${values[j]} -gt ${values[$((j+1))]} ]]
+                then
+                        # swap
+                        temp=${values[j]}
+                        values[$j]=${values[$((j+1))]}
+                        values[$((j+1))]=$temp
+                fi
+        done
+done
+
+echo "Array in Ascending order :"
+echo ${values[*]}
